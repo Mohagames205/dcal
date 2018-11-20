cc701095370f073ece4ffbe175fa5a97698c25f2
Mohamed El Yousfi <mohamedelyousfi2003@gmail.com>
2018-11-20 22:07:12 +0100

    Grote aanpassingen

--- dcal.py
+++ dcal.py
@@ -4,7 +4,7 @@ import sys
 vergelijking = input("Typ de vergelijking in ")
 test = re.split("[-+]", vergelijking)
 abc = vergelijking.split(" ")
-
+print(abc)
 term1 = test[0]
 term2 = test[1]
 term3 = test[2]
@@ -16,16 +16,71 @@ if term1[0].isalpha() == True:
     print("We kunnen nog niet rekenen met letters.")
     sys.exit()
 
-a = int(term1[0])
-b = int(term2[0])
-c = int(term3)
+if term1[0] == ' ':
+    a = 1
+    print("check1")
+    print (a)
+    b = int(term2[0])
+    c = int(term3)
+elif term2[0] == ' ':
+    b = 1
+    print("check2")
+    print (b)
+    a = int(term1[0])
+    c = int(term3)
+elif term3 == ' ':
+    c = 1
+    print("check3")
+    print (c)
+    a = int(term1[0])
+    b = int(term2[0])
+
+else:
+    a = int(term1[0])
+    b = int(term2[0])
+    c = int(term3)
+
+print (term1[0] + term2[0] + term3)
 
 if "-" in abc:
     if abc.count("-") == 2:
         c = c
     c = -c
+print (c)
 
 
 
-berekenen = b**2 - 4 * a * c
-print (berekenen)
+z = pow(b, 2)
+a_str = str(a)
+c_str = str(c)
+b_str = str(b)
+y = 4 * a * c
+print(y)
+ya = str(y)
+print (ya)
+if "-" in ya:
+
+    if abc.count("-") == 2:
+        ya = int(ya)
+        #ya = abs(ya)
+        berekenen = z + ya
+        print("plus weg, 2 minnen")
+
+    else:
+        z = str(z)
+        totaal = (z+ "-" + ya)
+        berekenen = eval(totaal)
+        print("min")
+else:
+    ya = int(ya)
+    ya = abs(ya)
+    berekenen = z + ya
+    print("plus 1 weg")
+
+print("========================")
+print(f"De discriminant is {berekenen}")
+b_sqr = math.sqrt(berekenen)
+b_sqrf = math.floor(b_sqr)
+
+print(f"De vierkantswortel van de discriminant is {b_sqr}")
+print("===========================")
