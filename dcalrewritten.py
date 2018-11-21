6d8638fd8f0b4851fe311801a2a6db649946cc07
Mohamed El Yousfi <mohamedelyousfi2003@gmail.com>
2018-11-21 14:53:33 +0100

    Update dcalrewritten.py
    code is vele korter!

--- dcalrewritten.py
+++ dcalrewritten.py
@@ -3,12 +3,13 @@ import re
 import sys 
 
 term1 = input("Typ de eerste term van de vergelijking in. ")
-term1 = term1.split("x²")[0]
+a = term1.split("x²")[0]
 if term1.isalpha() == True:
     print("Deze versie kan nog niet rekenen met letters!")
     sys.exit()
 
 term2 = input("Typ de tweede term van de vergelijking in. ")
+b = term2.split("x")[0]
 if term2.isalpha() == True:
     print("Deze versie kan nog niet rekenen met letters!")
     sys.exit()
@@ -18,12 +19,29 @@ if term3.isalpha() == True:
     print("Deze versie kan nog niet rekenen met letters!")
     sys.exit()
 
-term2 = term2.split("x")[0]
+c = term3
 
 if term1.isalpha() == True or term2.isalpha() == True or term3.isalpha() == True:
     print("We kunnen nog niet rekenen met letters.")
     sys.exit()
 
-print(term1)
-print(term2)
-print(term3)
+
+
+
+print(a)
+print(b)
+print(c)
+vergelijking = term1+term2+term3
+print(vergelijking)
+
+a = int(a)
+b = int(b)
+c = int(c)
+
+b_pow = pow(b, 2)
+print(b_pow)
+rest = 4 * a * c
+uitkomst = b_pow - rest
+print(rest)
+print (uitkomst)
+
